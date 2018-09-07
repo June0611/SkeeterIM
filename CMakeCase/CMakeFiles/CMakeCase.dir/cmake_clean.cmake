4b5e438c449f468e42753ac7ccb821190e70d0f2
June <1540502624@qq.com>
2018-09-07 10:12:21 +0800

    添加

---
+++ CMakeCase/CMakeFiles/CMakeCase.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CMakeCase.dir/main.cpp.o"
+  "CMakeCase.pdb"
+  "CMakeCase"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CMakeCase.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
